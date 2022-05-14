0a35634887dad4c210906aa8ff89915f332bae76
montesqieu <97889516+montesqieu@users.noreply.github.com>
2022-05-14 10:19:53 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,33 @@ import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Button1 from '../components/Button1'
 import Button2 from '../components/Button2'
+import Toggle from '../components/Toggle'
 import { graphql, Link } from 'gatsby'
 import {
           header_screen,
-          header,
+          header_box,
           header_info,
           header_img,
-          btn_box,
+          header_btn_box,
           btn_1,
           btn_2,
           btn,
-          second_screen,
+          client_screen,
           section_header,
           section_subheader,
           section_paragraph,
-          grid_box,
-          grid_item,
-          grid_icon,
+          client_grid_box,
+          client_grid_item,
+          client_grid_icon,
           value_screen,
-          testimonial_screen,
-          testimonial_grid,
-          testimonial_item,
-          testimonial_stars,
-          testimonial_text,
-          testimonial_client,
-          testimonial_profession,
-          testimonial_source,
+          ref_screen,
+          ref_grid,
+          ref_item,
+          ref_stars,
+          ref_text,
+          ref_client,
+          ref_profession,
+          price_screen
         }
         from '../styles/home.module.css'
 import { GiBackPain } from "@react-icons/all-files/gi/GiBackPain";
@@ -64,13 +65,13 @@ function shuffle(array) {
 
 export default function Home({ data }) {
   console.log(data)
-  const testimonial = shuffle(data.testimonial.nodes).slice(0, 4);
+  const reference = shuffle(data.reference.nodes).slice(0, 4);
   
   return (
     <Layout>
       <div className={header_screen}>
           <Navbar />
-        <div className={header}>
+        <div className={header_box}>
           <div className={header_img}>
             <GatsbyImage image={data.obraz.childImageSharp.gatsbyImageData} alt="banner"/>
           </div>
@@ -78,40 +79,40 @@ export default function Home({ data }) {
             <span>Odbuduj codzienną sprawność</span>
             <h1>Fizjoterapia i trening personalny zgrane z Twoimi potrzebami</h1>
             <p>Jestem wyspecjalizowanym fizjoterapeutą i trenerem personalnym, który koncentruje się na tym, aby pomóc Ci odzyskać sprawność i dobre samopoczucie.</p>
-            <div className={btn_box}>
+            <div className={header_btn_box}>
               <div className={btn_1}><Button1 /></div>           
               <div className={btn_2}><Button2 /></div>
             </div>
           </div>
         </div>
       </div>
-      <div className={second_screen} id="scroll">
+      <div className={client_screen} id="scroll">
         <h2 className={section_header}>Fizjoterapia i trening dla zasiedzianych</h2>
         <div className={section_subheader}>Czy jesteś we właściwym miejscu?</div>
         <p className={section_paragraph}>Fizjoterapia i trening to kompletny zestaw terapeutyczno-ruchowy, którego potrzebujesz. Skąd to wiem? Zbudowałem to miejsce dla osób takich jak Ty. Mogę się mylić, dlatego sprawdźmy czy faktycznie jesteś we właściwym miejscu. Jeśli chociaż jedno z poniższych stwierdzeń dotyczy Ciebie to zostajesz tu na dłużej i czytasz dalej, zgoda?</p>
-        <div className={grid_box}>
-          <div className={grid_item}>
-            <div className={grid_icon}><GiBackPain /></div>
+        <div className={client_grid_box}>
+          <div className={client_grid_item}>
+            <div className={client_grid_icon}><GiBackPain /></div>
             <p>cierpisz z powodu bólu pleców</p>
           </div>
-          <div className={grid_item}>
-            <div className={grid_icon}><FaUserInjured /></div>
+          <div className={client_grid_item}>
+            <div className={client_grid_icon}><FaUserInjured /></div>
             <p>nękają Cię kontuzje i/lub urazy</p>
           </div>
-          <div className={grid_item}>
-            <div className={grid_icon}><FaFlagCheckered /></div>
+          <div className={client_grid_item}>
+            <div className={client_grid_icon}><FaFlagCheckered /></div>
             <p>posiadasz ulotną motywację do ćwiczeń</p>
           </div>
-          <div className={grid_item}>
-            <div className={grid_icon}><FaRunning /></div>
+          <div className={client_grid_item}>
+            <div className={client_grid_icon}><FaRunning /></div>
             <p>Twoja technika ćwiczeń wymaga poprawy</p>
           </div>
-          <div className={grid_item}>
-            <div className={grid_icon}><GiShrimp /></div>
+          <div className={client_grid_item}>
+            <div className={client_grid_icon}><GiShrimp /></div>
             <p>irytuje Cię Twoja zgarbiona sylwetka</p>
           </div>
-          <div className={grid_item}>
-            <div className={grid_icon}><GiStopSign /></div>
+          <div className={client_grid_item}>
+            <div className={client_grid_icon}><GiStopSign /></div>
             <p>masz opór przed wykonywaniem ćwiczeń</p>
           </div>
         </div>
@@ -140,23 +141,41 @@ export default function Home({ data }) {
           <Button1 />
         </div>
       </div>
-      <div className={testimonial_screen}>
-        <div className={testimonial_grid}>
-          {testimonial.map(testimonial => (
-            <div className={testimonial_item}>
-              <div className={testimonial_stars}><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></div>
-              <div className={testimonial_text}>{testimonial.frontmatter.opinion}</div>
-              <div className={testimonial_client}>{testimonial.frontmatter.client}</div>
-              <div className={testimonial_profession}>{testimonial.frontmatter.profession}</div>
-              <div className={testimonial_source}>źródło: {testimonial.frontmatter.source}</div>
+      <div className={ref_screen}>
+        <div className={ref_grid}>
+          {reference.map(reference => (
+            <div className={ref_item}>
+              <div className={ref_stars}><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></div>
+              <div className={ref_text}>{reference.frontmatter.opinion}</div>
+              <div className={ref_client}>{reference.frontmatter.client}</div>
+              <div className={ref_profession}>{reference.frontmatter.profession}</div>
             </div>
             ))}
         </div>
       </div>
-      <div className={value_screen}>
+      <div className={price_screen}>
+        <div className={section_header}>Cennik</div>
+        <div className={section_subheader}>Odzyskaj kontrolę nad sprawnym ciałem</div>
+        <p className={section_paragraph}>Fizjoterapia i trening to kompletny zestaw terapeutyczno-ruchowy, którego potrzebujesz. Skąd to wiem? Zbudowałem to miejsce dla osób takich jak Ty. Mogę się mylić, dlatego sprawdźmy czy faktycznie jesteś we właściwym miejscu. Jeśli chociaż jedno z poniższych stwierdzeń dotyczy Ciebie to zostajesz tu na dłużej i czytasz dalej, zgoda?</p>
+        <Toggle>
+
+
+        <div className={value_screen}>
         <h2 className={section_header}>Fizjoterapia i trening - zestaw kompletny</h2>
         <div className={section_subheader}>Odzyskaj kontrolę nad sprawnym ciałem</div>
         <p className={section_paragraph}>Fizjoterapia i trening to kompletny zestaw terapeutyczno-ruchowy, którego potrzebujesz. Skąd to wiem? Zbudowałem to miejsce dla osób takich jak Ty. Mogę się mylić, dlatego sprawdźmy czy faktycznie jesteś we właściwym miejscu. Jeśli chociaż jedno z poniższych stwierdzeń dotyczy Ciebie to zostajesz tu na dłużej i czytasz dalej, zgoda?</p>
+        <div>test test test test test test test test test test test test </div>
+        <div>test test test test test test test test test test test test </div>
+        <div>test test test test test test test test test test test test </div>
+        <div>test test test test test test test test test test test test </div>
+        <div>test test test test test test test test test test test test </div>
+        <div>test test test test test test test test test test test test </div>
+        <div>test test test test test test test test test test test test </div>
+        <div>test test test test test test test test test test test test </div>
+      </div>
+
+
+        </Toggle>
         <div>test test test test test test test test test test test test </div>
         <div>test test test test test test test test test test test test </div>
         <div>test test test test test test test test test test test test </div>
@@ -184,7 +203,7 @@ query Home {
       gatsbyImageData(layout: FULL_WIDTH)
     }
   }
-  testimonial: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(testimonials)/"}}) {
+  reference: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(references)/"}}) {
     nodes {
       frontmatter {
         client
