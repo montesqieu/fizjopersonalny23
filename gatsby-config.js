cb084f134f037387fb6aa191ae18b324959417d3
montesqieu <97889516+montesqieu@users.noreply.github.com>
2022-05-18 18:50:29 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,13 @@ module.exports = {
         path: `${__dirname}/src/images/`,
        },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options : {
+         name: `services`,
+        path: `${__dirname}/src/images/services`,
+       },
+    },
   ],
   siteMetadata: {
     title: 'FizjoPersonalny',
